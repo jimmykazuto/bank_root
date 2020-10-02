dabcd55c3c8bb9097f140104d9b83118ad642303
Aurelie Lerouxel <malkiniou@gmail.com>
2020-10-02 09:57:24 +0200

    create sql tables for the application

--- sql/banque_php.sql
+++ sql/banque_php.sql
@@ -12,3 +12,20 @@ CREATE TABLE utilisateur
     code_postal VARCHAR (10) NOT NULL,
     ville VARCHAR (50) NOT NULL
 );
+CREATE TABLE compte
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    type_compte VARCHAR (30) NOT NULL,
+    numero_de_compte VARCHAR (30) NOT NULL,
+    creation_compte DATE NOT NULL
+);
+CREATE TABLE operation
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    type_operation VARCHAR (50) NOT NULL,
+    montant_operation INT NOT NULL,
+    date_operation DATETIME NOT NULL,
+    solde_compte INT NOT NULL,
+    compte_origine VARCHAR (50) NOT NULL,
+    compte_destinataire VARCHAR (50) NOT NULL
+);
