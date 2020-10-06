717c310661d5bf762fefdc05502e039a0c96c382
Aurelie Lerouxel <malkiniou@gmail.com>
2020-10-06 18:04:29 +0200

    create fk

--- sql/banque_php.sql
+++ sql/banque_php.sql
@@ -1,62 +1,70 @@
--- delete the user if it is already present in the database
-DROP USER IF EXISTS "banquePHP'@'localhost";
-
--- create a user account with management rights on the database
-CREATE USER "banquePHP'@'localhost" IDENTIFIED BY 'banquePHP';
-
 -- delete the database if it already exists
 DROP DATABASE banque_php;
 
 -- create a database 
 CREATE DATABASE banque_php DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
--- give rights to the created user
-GRANT ALL ON `banque_php`.* TO 'banquePHP'@'localhost';
+USE banque_php;
 
-CREATE TABLE Users
+-- delete the user if it is already present in the database
+DROP USER IF EXISTS "banquePHP'@'localhost";
+
+-- create a user account with management rights on the database
+CREATE USER "banquePHP'@'localhost";
+
+-- give rights to the created user
+GRANT ALL PRIVILEGES ON banque_php.* To 'banquePHP'@'localhost' IDENTIFIED BY 'banquePHP';
+
+CREATE TABLE User
 (
-    UserID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    UserName VARCHAR (30) NOT NULL,
-    UserLastName VARCHAR (30) NOT NULL,
-    UserEmail VARCHAR (50) NOT NULL,
-    UserPassword VARCHAR (15) NOT NULL,
-    UserCountry VARCHAR (50) NOT NULL,
-    UserAddress VARCHAR (50) NOT NULL,
-    UserPostalCode VARCHAR (10) NOT NULL,
-    UserCity VARCHAR (50) NOT NULL
-);
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR (50) NOT NULL,
+    lastname VARCHAR (50) NOT NULL,
+    birth_date DATE NOT NULL,
+    email VARCHAR (50) NOT NULL UNIQUE,
+    password VARCHAR (255) NOT NULL,
+    country VARCHAR (50) NOT NULL,
+    adress VARCHAR (50) NOT NULL,
+    postal_code CHAR (10) NOT NULL,
+    city VARCHAR (50) NOT NULL,
+    PRIMARY KEY (id)
+)
+ENGINE=InnoDB;
 -- creation of data rows in the table
-INSERT INTO Users (UserName, UserLastName, UserEmail, UserPassword, UserCountry, UserAddress, UserPostalCode, UserCity)
-VALUES ('lerouxel', 'aurelie', 'aurelielerouxel@cacraint.fr', 'lol', 'france', '9 rue du buisson', 27180, 'st sebastien du mordor'),
-    ('Penneflamme', 'Katty', 'tunes@pas.fr', 'i8m2p', 'france', '2 notre', 01010, 'galaxie');
+INSERT INTO User (firstname, lastname, birth_date, email, password, country, adress, postal_code, city)
+VALUES ('Lerouxel', 'Aurelie',"1981-08-19", 'aurelielerouxel@cacraint.fr', 'lol', 'france', '9 rue du buisson', 27180, 'st sebastien du mordor'),
+    ('Penneflamme', 'Katty', "1978-11-07", 'tunes@pas.fr', 'i8m2p', 'france', '2 notre', 01010, 'galaxie');
 
 CREATE TABLE Account
 (
-    AccountID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    AccountType VARCHAR (30) NOT NULL,
-    AccountNumber VARCHAR (30) NOT NULL,
-    AccountCreationDate DATE NOT NULL
-);
-INSERT INTO Account (AccountType, AccountNumber, AccountCreationDate)
-VALUES ('livret_a', '0256kih2487', '2009/10/02'),
-    ('compte_courant', '2489fsf5487', '2006/08/19'),
-    ('compte_courant', '1464lol7458', '2001/04/24');
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    account_type VARCHAR (50) NOT NULL,
+    account_number VARCHAR (30) NOT NULL,
+    amount DECIMAL(11,2) NOT NULL,
+    opening_date DATE NOT NULL,
+    user_id INT UNSIGNED, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES User(id)
+)
+ENGINE=InnoDB;
+INSERT INTO Account (account_type, user_id, account_number, amount, opening_date)
+VALUES ('livret_a', 2, '0256kih2487', -435.25, '2009/10/02'),
+    ('compte_courant', 2, '2489fsf5487', 147.58, '2006/08/19'),
+    ('compte_courant', 1, '1464lol7458', 1435.25, '2001/04/24');
 
 CREATE TABLE Operation
 (
-    OperationID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    OperationType VARCHAR (50) NOT NULL,
-    OperationPrice INT NOT NULL,
-    OperationDate DATETIME NOT NULL,
-    Account_balance INT NOT NULL,
-    AccountDebitOriginal VARCHAR (50),
-    AccountRecipientCredit VARCHAR (50)
-
-);
-INSERT INTO Operation (OperationType, OperationPrice, OperationDate, Account_balance, AccountDebitOriginal, AccountRecipientCredit)
-VALUES ('transfert', -1250, '2019/10/03 21:56:12', 1247.20,' livret_a', 'compte_courant'),
-    ('leclerc', -60.25, '2020/10/01 14:01:32', 1624.68, 'compte_courant', 'NULL');
-
--- ALTER TABLE Operation
--- ADD FOREIGN KEY (AccountID) REFERENCES
--- Account (AccountID);
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    operation_type VARCHAR (50) NOT NULL,
+    account_id INT UNSIGNED,
+    amount DECIMAL(11,2) NOT NULL,
+    operation_date TIMESTAMP NOT NULL,
+    account_balance INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (account_id) REFERENCES Account(id)
+)
+ENGINE=InnoDB;
+INSERT INTO Operation (operation_type, account_id, amount, operation_date, account_balance)
+VALUES ('totol essence', 2, -65.25, NOW(), 3200.14),
+    ('paye octobre', 2, 2596.36, NOW(), 5796.5),
+    ('leclerc', 1, -40, NOW(), 1569.24);
